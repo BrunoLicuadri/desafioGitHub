b33e3847c7bd24ced7a23974a5d1c1185dd9d7b6
Bruno Licuadri <blicuadri@gmail.com>
2022-08-23 19:39:58 -0300

    Commitando segunda vez.

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Não sei o que fazer então por enquanto será apenas Commit 1.");
+		System.out.println();
+		System.out.println("Queria fazer alguma coisa m ais significativa mas to sem ideia então....");
+		System.out.println("para n perder tempo que quero avancar logo aprender logo,");
+		System.out.println("commit nele ^^");
+		System.out.println();
 
 	}
 
