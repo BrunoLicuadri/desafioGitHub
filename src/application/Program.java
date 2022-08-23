b4b324b7741f77e280f7c61f2c60f5efff56d386
Bruno Licuadri <blicuadri@gmail.com>
2022-08-23 19:27:20 -0300

    versao/Commit 1 desafio Git & GitHub.

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Não sei o que fazer então por enquanto será apenas Commit 1.");
+
+	}
+
+}
